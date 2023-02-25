3cd69521d63dfcf9b3052a4fd76d03b9452f23eb
Your Name <betancur10nicolas@gmail.com>
2023-02-25 17:28:56 -0500

    add

--- Controllers/Controlador.js
+++ Controllers/Controlador.js
@@ -2,17 +2,26 @@ import { ServiciosGet } from '../services/get.js'
 
 export class Controlador {
     constructor() { }
-
     async datosMostrar(request, response) {
+
         let id_guia = request.params.id;
 
         let servicios = new ServiciosGet()
-
         try {
-            response.status(200).json({
-                "mensaje": "Exito en la consulta",
-                "datos": await servicios.traerData(id_guia)
-            })
+            const datos = await servicios.traerData(id_guia)
+            const dataValidador = Object.values(datos).some(valor => valor === null || valor === undefined);
+            if (dataValidador == false) {
+                response.status(200).json({
+                    "mensaje": "Exito en la consulta",
+                    "datos": await servicios.traerData(id_guia)
+                })
+            }
+            else {
+                response.status(400).json({
+                    "mensaje": "Error en la consulta no encontra la guia" + error,
+                    "datos": ':(',
+                })
+            }
 
         } catch (error) {
             response.status(400).json({
